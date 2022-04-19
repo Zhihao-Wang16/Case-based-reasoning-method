11ed7867770a0fb0f7b94289d72622b17f18a552
Zhihao-Wang16 <60289894+Zhihao-Wang16@users.noreply.github.com>
2022-04-19 13:42:56 +0200

    Update Similarity_functions.R

--- scripts/Similarity_functions.R
+++ scripts/Similarity_functions.R
@@ -15,8 +15,8 @@
 
 # @Example
 # Spatial resolution between Ecuador with 10m resolution and Bologna with 25m resolution
-# # source_area = 10
-# # target_area = 25
+# # source_area$resolution = 10
+# # target_area$resolution = 25
 # # RESOLUTION_SIMILARITY_FUN(source_area, target_area)
 
 
@@ -24,9 +24,9 @@
 
 RESOLUTION_SIMILARITY_FUN <- function(source_area, target_area){
 
-  if (source_area <= target_area) {
+  if (source_area$resolution <= target_area$resolution) {
 
-    sim_resolution <- 2 ^ -(2 * abs(log10(source_area)-log10(target_area))) ^ 0.5
+    sim_resolution <- 2 ^ -(2 * abs(log10(source_area$resolution)-log10(target_area$resolution))) ^ 0.5
 
   }else{
 
@@ -42,7 +42,7 @@ RESOLUTION_SIMILARITY_FUN <- function(source_area, target_area){
 
 TOTALRELIEF_SIMILARITY_FUN <- function(source_area, target_area){
 
-  sim_totalrelief <- 1-(abs(source_area - target_area) / max(8848-source_area, source_area))
+  sim_totalrelief <- 1-(abs(source_area$total_relief - target_area$total_relief) / max(8848-source_area$total_relief, source_area$total_relief))
 
   return(sim_totalrelief)
 
@@ -55,7 +55,7 @@ MEANSLOPE_SIMILARITY_FUN <- function(source_area, target_area, limit_Slope){
   # Args:
   #   limit_slope: should cover mean slope of each study area, in our paper is 40.
 
-  sim_meanslope <- 1 - (abs(source_area - target_area) / max(limit_slope-source_area, source_area))
+  sim_meanslope <- 1 - (abs(source_area$mean_slope - target_area$mean_slope) / max(limit_slope-source_area$mean_slope, source_area$mean_slope))
 
   return(sim_meanslope)
 
@@ -65,7 +65,7 @@ MEANSLOPE_SIMILARITY_FUN <- function(source_area, target_area, limit_Slope){
 
 STDSLOPE_SIMILARITY_FUN <- function(source_area, target_area){
 
-  sim_stdslope <-  2 ^ -(2*abs(log10(source_area)-log10(target_area))) ^ 0.5
+  sim_stdslope <-  2 ^ -(2*abs(log10(source_area$std_slope)-log10(target_area$std_slope))) ^ 0.5
 
   return(sim_stdslope)
 
@@ -74,12 +74,14 @@ STDSLOPE_SIMILARITY_FUN <- function(source_area, target_area){
 ### GEOLOGICAL UNITS
 
 GEOLOGICALUNITS_SIMILARITY_FUN <- function(source_area, target_area){
+  
+  
 
-  Igneous_sim = !is.null(intersect(source_area$Igneous, target_area$Igneous))
+  Igneous_sim = identical(source_area$Igneous, target_area$Igneous)
 
-  Sedimentary_sim = !is.null(intersect(source_area$Sedimentary, target_area$Sedimentary))
+  Sedimentary_sim = identical(source_area$Sedimentary, target_area$Sedimentary)
 
-  Metamorphic_sim = !is.null(intersect(source_area$Metamorphic, target_area$Metamorphic))
+  Metamorphic_sim = identical(source_area$Metamorphic, target_area$Metamorphic)
 
   sim_geounits = mean(c(Igneous_sim, Sedimentary_sim, Metamorphic_sim))
 
